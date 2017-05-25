6ef198a93b25794273c6d05fa637dc135fcac6ad
Marcos Flavio <marcosflaviosilveira@gmail.com>
2017-05-25 15:06:51 -0300

    Sample example of retrieving data from MySQL via python

--- connection/simpleConnection.py
+++ connection/simpleConnection.py
@@ -1,10 +1,11 @@
 import MySQLdb
 import sys
-connection = MySQLdb.connect(host = "localhost", user = "root", passwd = "root", db = "ptstack")
+connection = MySQLdb.connect(host = "localhost", user = "root", passwd = "root", db = "teste")
 cursor = connection.cursor()
-cursor.execute("SELECT VERSION()")
-row = cursor.fetchone()
-print "Versao do MySQL", row[0]
+cursor.execute ("select name_first, name_last from address")
+data = cursor.fetchall ()
 cursor.close()
 connection.close()
+for row in data :
+    print row[0], row[1]
 sys.exit()
