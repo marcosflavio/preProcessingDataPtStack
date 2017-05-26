935e48f491a503ad551c316446f476b80f3a7688
Marcos Flavio <marcosflaviosilveira@gmail.com>
2017-05-25 23:11:42 -0300

    Add PortugueseProcess.py class from sentText Framework

--- preProcess/main.py
+++ preProcess/main.py
@@ -1,7 +1,12 @@
+from portugueseProcess import RegexpReplacer
 from connectionFactory import MySqlConnector
+from portugueseProcess import TextCleaner
 
-mysql = MySqlConnector()
+#mysql = MySqlConnector()
 
-mysql.create_connection("localhost", "root", "root", "teste")
-mysql.create_cursor()
-mysql.set_query("insert into address(name_first) values ('marcosfive')")
+#mysql.create_connection("localhost", "root", "root", "teste")
+#mysql.create_cursor()
+#mysql.set_query("insert into address(name_first) values ('marcosfive')")
+
+#textCleaner = TextCleaner()
+#print textCleaner.removeSufPort("cachorrinho")
