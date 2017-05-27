3f82039768ca8b78c9c3e9df3da01795bd6e76d1
Marcos Flavio <marcosflaviosilveira@gmail.com>
2017-05-27 16:44:18 -0300

    Pre processing class created

--- preProcess/main.py
+++ preProcess/main.py
@@ -1,4 +1,4 @@
-from portugueseProcess import RegexpReplacer
+# -*- coding: latin-1 -*-
 from connectionFactory import MySqlConnector
 from portugueseProcess import TextCleaner
 
@@ -6,7 +6,8 @@ from portugueseProcess import TextCleaner
 
 #mysql.create_connection("localhost", "root", "root", "teste")
 #mysql.create_cursor()
-#mysql.set_query("insert into address(name_first) values ('marcosfive')")
-
-#textCleaner = TextCleaner()
-#print textCleaner.removeSufPort("cachorrinho")
+#mysql.set_query("insert into address(name_first) values ('MA¡¡¡¡¡¡RCOS')")
+#data = mysql.get_result_by_query("select name_first from address where name_first = 'MA¡¡¡¡¡¡RCOS'")
+#textCleaner = TextCleaner(False)
+#for w in data:
+    #print textCleaner.removeStopwords(u"como m·rcos fl·vio sÌlveira ·lves")
--- preProcess/preProcessingProcess.py
+++ preProcess/preProcessingProcess.py
@@ -1,1 +1,58 @@
-#TODO preProcess methods
+from question import Question
+
+
+class PreProcessing:
+
+    def __init__(self, posts, textCleaner):
+        self.posts = posts
+        self.questions = None
+        self.textCleaner = textCleaner
+
+    def create_questions(self):
+        for p in self.posts:
+            q = Question(p[1], p[0])
+            self.questions.append(q)
+        self.posts = None
+        return self.questions
+
+    # removeRepChar
+    def remove_rep_char(self):
+        for q in self.questions:
+            q.set_title(self.textCleaner.removeRepChar(q.get_title()))
+        return self.questions
+
+    # removeSymbols
+    def remove_symbols(self):
+        for q in self.questions:
+            q.set_title(self.textCleaner.removeSymbols(q.get_title()))
+        return self.questions
+
+    # removeSufPort
+    def remove_sufix_portugues(self):
+        for q in self.questions:
+            q.set_title(self.textCleaner.removeSufPort(q.get_title()))
+        return self.questions
+
+    # removeAccent
+    def remove_accent(self):
+        for q in self.questions:
+            q.set_title(self.textCleaner.removeAccent(q.get_title()))
+        return self.questions
+
+    # removeLinks
+    def remove_links(self):
+        for q in self.questions:
+            q.set_title(self.textCleaner.removeLinks(q.get_title()))
+        return self.questions
+
+    # removeStopwords
+    def remove_stop_words(self):
+        for q in self.questions:
+            q.set_title(self.textCleaner.removeStopwords(q.get_title()))
+        return self.questions
+
+    def get_questions(self):
+        return self.questions
+
+    def set_questions(self, questions):
+        self.questions = questions
