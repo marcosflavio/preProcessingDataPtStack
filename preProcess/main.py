2831c00807d28b4bc6c539149db4a964ea4fe44e
Marcos Flavio <marcosflaviosilveira@gmail.com>
2017-06-01 16:42:12 -0300

    Insert data

--- preProcess/main.py
+++ preProcess/main.py
@@ -7,15 +7,33 @@ mysql = MySqlConnector()
 
 mysql.create_connection("localhost", "root", "root", "ptstack")
 mysql.create_cursor()
-#mysql.set_query("insert into address(name_first) values ('MA¡¡¡¡¡¡RCOS')")
 data = mysql.get_result_by_query("select id , title from posts where post_type_id = 1")
+mysql.close_connection()
+mysql.close_cursor()
+
 textCleaner = TextCleaner(False)
 preProcess = PreProcessing(data, textCleaner)
 preProcess.create_questions()
 
 preProcess.remove_accent()
 preProcess.remove_links()
-qs = preProcess.remove_rep_char()
+preProcess.remove_rep_char()
 preProcess.remove_symbols()
 preProcess.remove_stop_words()
-preProcess.remove_sufix_portugues()
+qs = preProcess.remove_sufix_portugues()
+
+mysql.create_connection("localhost", "root", "root", "ptstack")
+mysql.create_cursor()
+cursor = mysql.get_cursor()
+con = mysql.get_connection()
+for q in qs:
+    title = q.get_title()
+    id = q.get_post_id()
+    try:
+        sql = "INSERT INTO questions (title, post_id) VALUES ('" + title + "'," + str(id) + ")"
+        cursor.execute(sql)
+        con.commit()
+    except:
+        con.rollback()
+mysql.close_cursor()
+mysql.close_connection()
